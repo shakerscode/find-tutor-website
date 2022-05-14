d383843c6ffd2e6a4dd65211be7f01da5e3fb23a
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-05-14 16:57:18 +0600

    all edit and issues are  done

--- src/Pages/AppointTutor/AppointBanner.js
+++ src/Pages/AppointTutor/AppointBanner.js
@@ -5,9 +5,9 @@ import appointBanner from '../../images/appoint-banner.png'
 const AppointBanner = () => {
     return (
         <div class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={appointBanner} class="max-w-sm rounded-lg shadow-2xl " alt='' />
-                <div className='text-secondary px-12'>
+            <div class="hero-content flex-col flex-col-reverse lg:flex-row-reverse">
+                <img src={appointBanner} class="lg:max-w-sm max-w-xs rounded-lg shadow-2xl " alt='' />
+                <div className='text-secondary px-12 mt-8'>
                     <h1 class="text-3xl  font-bold">Appoint A Best Tutor For You!</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda <br /> excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button class="btn btn-primary text-white bg-gradient-to-r from-purple-400 to-primary-800">Get Started</button>
