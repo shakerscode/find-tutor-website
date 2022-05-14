6d667add878db162651635ade8b6e0b559b043c5
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-05-14 16:53:01 +0600

    all edit  done

--- src/Pages/AppointTutor/AppointTutor.js
+++ src/Pages/AppointTutor/AppointTutor.js
@@ -7,7 +7,7 @@ const AppointTutor = () => {
         <div>
             <AppointBanner></AppointBanner>
             <div>
-                <h1 className='text-3xl text-center font-bold text-primary'>Choose A Date To Start</h1>
+                <h1 className='text-3xl text-center font-bold text-primary mt-3'>Choose A Date To Start</h1>
                 <Calender></Calender>
             </div>
         </div>
--- src/Pages/AppointTutor/Calender.js
+++ src/Pages/AppointTutor/Calender.js
@@ -7,12 +7,12 @@ const Calender = () => {
     const [date, setDate] = useState(new Date());
     return (
         <div class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <div class="text-center w-full lg:text-center text-secondary px-10 mx-10">
+            <div class="hero-content flex-col-reverse lg:flex-row-reverse">
+                <div class="text-center w-full lg:text-center text-secondary lg:px-10 lg:mx-10">
                     <h1 class="text-3xl font-bold text-center ">Get Your Date</h1>
                     <p class="py-6 text-xl font-semibold text-center text-primary">Your Selected Date Is {format(date, 'PP')} </p>
                 </div>
-                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 text-secondary">
+                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 text-secondary mb-20">
                    <DayPicker
                    mode="single"
                    selected={date}
